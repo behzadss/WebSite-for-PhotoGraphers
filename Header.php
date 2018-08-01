9dbedde4241eafe073ebc045dd2e18d936efd3d1
behzas ss <behzadspacey@gmail.com>
2018-08-01 11:58:58 +0430

    upload All files

---
+++ Header.php
@@ -0,0 +1,18 @@
+
+<div id="fh5co-header">
+<div class="container">
+    <!-- Mobile Toggle Menu Button -->
+    <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+    <div id="fh5co-logo">
+        <a href="Home">Saaede DoostBekheir</a>
+    </div>
+    <nav id="fh5co-main-nav">
+        <ul>
+        <li><a href="Home" class="transition" data-nav-section="home">Home</a></li>
+            <li><a href="Home#.portfolio" data-nav-section="portfolio">Portfolio</a></li>
+            <li><a href="Home#about"  data-nav-section="about">About</a></li>
+            <li><a href="Home#contact"  data-nav-section="contact">Contact</a></li>
+        </ul>
+    </nav>
+</div>
+</div>
---
+++ Home.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Saaede DoostBekheir &mdash;</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="saaede doostbekheir" />
+	<meta name="keywords" content="d" />
+	<meta name="author" content="f" />
+
+  
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" type="image/png" href="myicon.png">
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:400,300,700' rel='stylesheet' type='text/css'>
+	<!-- Animate -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Flexslider -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Icomoon -->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	
+	<div id="fh5co-header">
+<div class="container">
+    <!-- Mobile Toggle Menu Button -->
+    <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+    <div id="fh5co-logo">
+        <a href="#">Saaede DoostBekheir</a>
+    </div>
+    <nav id="fh5co-main-nav">
+        <ul>
+        <li><a href="#"  data-nav-section="home">Home</a></li>
+            <li><a href="Home#.portfolio" data-nav-section="portfolio">Portfolio</a></li>
+            <li><a href="Home#about"  data-nav-section="about">About</a></li>
+            <li><a href="Home#contact"  data-nav-section="contact">Contact</a></li>
+        </ul>
+    </nav>
+</div>
+</div>
+	<div id="fh5co-main">
+		<div class="fh5co-overlay-mobile"></div>
+		<div id="fh5co-home" class="js-fullheight" data-section="home">
+		
+
+			<div class="flexslider">
+				
+				<div class="fh5co-overlay"></div>
+				<div class="fh5co-text">
+					<div class="container">
+						<div class="row text-center">
+							<!--<h1 class="animate-box">We love to tell your story</h1>-->
+							<div class="fh5co-go animate-box">
+								<a href="#" class="js-fh5co-next"><br><br><br>
+									See Portfolio
+									<br><br><br><span><i class="icon-arrow-down2"></i></span>
+								</a>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			  	<ul class="slides">
+			   	<li style="background-image: url(images/p2.jpg); background-position:center center; background-size:cover;" ></li>
+			   	<li style="background-image: url(images/p1.jpg); background-position:center center; background-size:cover;" ></li>
+			   	<li style="background-image: url(images/p3.jpg); background-position:center center; background-size:cover;" ></li>
+			  	</ul>
+
+			</div>
+		</div>
+		<div id=".portfolio">
+		<div id="fh5co-portfolio" data-section="portfolio">
+			<div class="container">
+				<div class="row r-pb">
+					<div class="col-md-8 col-md-offset-2 text-center section-heading">
+						<h2 class="fh5co-section-title animate-box">Portfolio</h2>
+						<p class="fh5co-lead animate-box">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4 col-sm-4 col-xs-6 col-xxs-12 animate-box">
+						<div class="img-grid">
+							<img src="images/p1.jpg" alt="behzad" class="img-responsive">
+							<a href="Album1" >
+								<div>
+									<span class="fh5co-meta">15 images</span>
+									<h2 class="fh5co-title">Album 1</h2>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 col-xxs-12 animate-box">
+						<div class="img-grid">
+							<img src="images/p2.jpg" alt="behzad" class="img-responsive">
+							<a href="Album2" >
+								<div>
+									<span class="fh5co-meta">8 images</span>
+									<h2 class="fh5co-title">Album 2</h2>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 col-xxs-12 animate-box">
+						<div class="img-grid">
+							<img src="images/p3.jpg" alt="behzad" class="img-responsive">
+							<a href="Album3" >
+								<div>
+									<span class="fh5co-meta">11 images</span>
+									<h2 class="fh5co-title">Album 3</h2>
+								</div>
+							</a>
+						</div>
+					</div>
+
+				
+
+				</div>
+			</div>
+		</div>
+	</div>
+		<div id="fh5co-about" data-section="about">
+			<div class="container">
+				<div class="row r-pb">
+					<div class="col-md-8 col-md-offset-2 text-center section-heading animate-box">
+						<h2 id='about' class="fh5co-section-title">About</h2>
+						<p class="fh5co-lead">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 animate-box">
+						<div class="fh5co-text ">
+							<h2>Saaede DoostBekheir</h2>
+							<span class="fh5co-meta">Photographer, Video Editor</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						</div>
+					</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		
+<div id='contact'> 
+		<div id="fh5co-contact" data-section="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center section-heading">
+						<h2  class="fh5co-section-title animate-box">Contact</h2>
+						<p class="animate-box">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						<p class="animate-box">	<ul class="fh5co-social">
+							<li>
+								<a href="http://saaede.tumblr.com/"><i class="icon-tumblr"></i></a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/saaede_d/"><i class="icon-instagram"></i></a>
+							</li>
+							<li>
+								<a href="mailto:saaede.db@gmail.com"><i class="icon-mail"></i></a>
+							</li>
+		
+							
+						</ul></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+
+		<div id="fh5co-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 animate-box">
+						<div class="fh5co-footer-widget">
+							<p>All images &copy;2018 Saaede DoostBekheir . All Rights Reserved.</p>
+							<p>Designed by behzad soleimani  </p>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 animate-box">
+						<div class="fh5co-footer-widget">
+						
+						
+				</div>
+			</div>
+		</div>
+
+	</div> <!-- END fh5co-page -->
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Main JS -->
+	<script src="js/bootstrap.min.js"></script>
+
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
